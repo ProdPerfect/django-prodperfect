29e01e282422c5ad1e463a896fa8aafba2e905cb
Lemuel Boyce <lemuelboyce@gmail.com>
2019-08-06 15:12:46 -0400

    change how prodperfect_options are marked safe
    Signed-off-by: Lemuel Boyce <lemuelboyce@gmail.com>

--- prodperfect/templatetags/prodperfect.py
+++ prodperfect/templatetags/prodperfect.py
@@ -6,6 +6,7 @@ Created by Lemuel Boyce on 2019-07-15
 import json
 
 from django import template
+from django.utils.safestring import mark_safe
 
 from prodperfect.settings import settings
 
@@ -19,5 +20,5 @@ def tracking_snippet(context):
     context['prodperfect_project_id'] = settings.PROJECT_ID
     context['prodperfect_tracking_lib'] = settings.TRACKING_LIBRARY_URL
     context['prodperfect_request_type'] = settings.REQUEST_TYPE
-    context['prodperfect_options'] = json.dumps(settings.OPTIONS)
+    context['prodperfect_options'] = mark_safe(json.dumps(settings.OPTIONS))
     return context
